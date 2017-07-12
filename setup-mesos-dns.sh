d34060e6ba63f883449ea8a625f9b016e886cca8
Devesh Chanchlani <devesh.chanchlani@gmail.com>
2017-07-12 16:39:14 +0530

    Adding script to setup mesos-dns

---
+++ setup-mesos-dns.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 mesos-master IP address." >&2
+  echo "Usage: $1 mesos-slave IP address, hosting mesos-dns." >&2
+  exit 1
+fi
+
+mkdir /usr/local/mesos-dns/
+wget -O /usr/local/mesos-dns/mesos-dns https://github.com/mesosphere/mesos-dns/releases/download/v0.6.0/mesos-dns-v0.6.0-linux-amd64
+chmod a+x /usr/local/mesos-dns/mesos-dns
+
+sed -i "s/master-ip/$1/g" mesos-dns-config.json
+mv mesos-dns-config.json /usr/local/mesos-dns/.
+
+sudo /usr/local/mesos-dns/mesos-dns -config=/usr/local/mesos-dns/mesos-dns-config.json &
+
+sudo sed -i "1s/^/nameserver $2\n /" /etc/resolv.conf
